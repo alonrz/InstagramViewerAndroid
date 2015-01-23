c214578fe100bfb2441d45007c227fe72a0f6464
Alón Reich Zilberman <alonrz@yahoo-inc.com>
2015-01-22 23:21:51 -0800

    Implemented the ViewHolder Pattern to speed up listView

--- app/src/main/java/com/example/alonrz/instagramviewer/PostsAdapter.java
+++ app/src/main/java/com/example/alonrz/instagramviewer/PostsAdapter.java
@@ -19,6 +19,13 @@ import java.util.ArrayList;
  */
 public class PostsAdapter extends ArrayAdapter<InstagramPost> {
 
+    public static class ViewHolder
+    {
+        ImageView ivPhoto;
+        TextView tvLikes;
+        TextView tvCaption;
+        ImageView ivProfilePic;
+    }
     public PostsAdapter(Context context, ArrayList<InstagramPost> posts) {
         super(context, R.layout.item_photo, posts);
     }
@@ -26,24 +33,36 @@ public class PostsAdapter extends ArrayAdapter<InstagramPost> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         InstagramPost post = getItem(position);
+        ViewHolder viewHolder;
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+
+            viewHolder.ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+            viewHolder.tvLikes = (TextView) convertView.findViewById(R.id.tvLikes);
+            viewHolder.tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+            viewHolder.ivProfilePic = (ImageView) convertView.findViewById(R.id.ivProfilePic);
+            convertView.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder = (ViewHolder)convertView.getTag();
         }
         // Lookup view for data population
-        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
-        TextView tvLikes = (TextView) convertView.findViewById(R.id.tvLikes);
-        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
-        ImageView ivProfilePic = (ImageView) convertView.findViewById(R.id.ivProfilePic);
 
         // Populate the data into the template view using the data object
         String formattedUserNameAndCaption = "<b>"+post.getUsername()+"</b> " +
                 "<i>"+post.getCaption()+"</i>";
-        tvCaption.setText(Html.fromHtml(formattedUserNameAndCaption));
-        tvLikes.setText(Integer.toString(post.getLikes()));
-        ivPhoto.setImageResource(0);//reset the image view
-        Picasso.with(getContext()).load(post.getPhotoUrl()).placeholder(R.drawable.image_loading).error(R.drawable.error_icon).into(ivPhoto); //ivPhoto.set...;
-        Picasso.with(getContext()).load(post.getProfilePicture()).into(ivProfilePic);
+        viewHolder.tvCaption.setText(Html.fromHtml(formattedUserNameAndCaption));
+        viewHolder.tvLikes.setText(Integer.toString(post.getLikes()));
+        viewHolder.ivPhoto.setImageResource(0);//reset the image view
+        Picasso.with(getContext())
+                .load(post.getPhotoUrl())
+                .placeholder(R.drawable.image_loading)
+                .error(R.drawable.error_icon)
+                .into(viewHolder.ivPhoto); //ivPhoto.set...;
+        Picasso.with(getContext()).load(post.getProfilePicture()).into(viewHolder.ivProfilePic);
         // Return the completed view to render on screen
         return convertView;
     }
